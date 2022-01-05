e3e44c1523ea08b03b94b5e159406e0f4e9b4d2a
jumashafara <jumashafara0@gamil.com>
2022-01-05 14:01:37 +0300

    enabled search from multiple datasets

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+
+The search function takes in products in a string form, example "milk,bread,coffee,potatoes"
+and takes in one store and returns the full names of the products in a list. 
+Try for example: Store =  pnp , product = milk,jacobs
